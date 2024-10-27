b84c9a03860bb8dcf6520ec41dcf3e46ff12c9b4
CarolPachecoPozo <caro.pacheco@duocuc.cl>
2024-10-26 22:23:50 -0300

    funcion teclado

--- src/app/componentes/toolbar/toolbar.component.ts
+++ src/app/componentes/toolbar/toolbar.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { addIcons } from 'ionicons';
-import { create, ellipsisHorizontal, ellipsisVertical, helpCircle, personCircle, search, star } from 'ionicons/icons';
 import { Router } from '@angular/router';
+import { Keyboard } from '@capacitor/keyboard';
 
 @Component({
   selector: 'app-toolbar',
   templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss'],
+  styleUrls: ['./toolbar.component.scss']
 })
 export class ToolbarComponent  implements OnInit {
-
-  constructor(private router: Router) {
-    addIcons({ create, ellipsisHorizontal, ellipsisVertical, helpCircle, personCircle, search, star });
-  }
   perfil: string | null = null;
+  tecladoAbierto: boolean = false;
+
+  constructor(
+    private router: Router
+  ) {}
+
 
   ngOnInit() {
     this.perfil = localStorage.getItem('perfil');
+
+    //listener para cuando el teclado se muestra o no 
+    Keyboard.addListener('keyboardWillShow',()=>{
+      this.tecladoAbierto = true;
+    })
+
+    Keyboard.addListener('keyboardWillHide',()=>{
+      this.tecladoAbierto = false;
+    })
   }
 
   navPagina(page: string){
@@ -24,4 +34,6 @@ export class ToolbarComponent  implements OnInit {
       location.reload();
     })
   }
+
+  
 }
